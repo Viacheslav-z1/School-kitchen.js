bba9885ae621863db0faddcffe32685d5687a220
slavik <slavik.zaliotnyi@gmail.com>
2023-01-17 01:03:54 +0200

    added renders functions, adaptive main screen and modal form

--- js/app.js
+++ js/app.js
@@ -1,61 +1,134 @@
-tray = document.querySelector(".app__tray");
-foods = document.querySelectorAll(".app__food-item");
+const trayBox = document.querySelector(".app__tray-box");
+const trayInner = document.querySelector(".app__tray");
+const foodsList = document.querySelector(".app__food-inner");
+const totalPriceNode = document.querySelector(".app__price__number-num");
 
-foods.forEach(food => {
-  /*Додаєм клас елементу, якого перетягуєм, для його ідентифікації*/
-  food.addEventListener("dragstart", () => {
-    food.classList.add("drag");
-  });
-  /*Забираєм клас елементу, якого перетягуєм*/
-  food.addEventListener("dragend", () => {
-    food.classList.remove("drag");
-  });
-});
-/*Якась важлива штука, без якої не працює драг */
-tray.addEventListener("dragover", function (event) {
-  event.preventDefault();
-});
-
-/*Логіка додавання в корзину */
-tray.addEventListener('drop',(e)=>{
-  e.preventDefault();
-  const dragEl = document.querySelector(".drag");
-  const img =  dragEl.querySelector('img');
-  console.log(img);
-  let clone = img.cloneNode(true);
-  tray.appendChild(clone);
-})
+let trayArr = [];
 
 /*Масив товарів */
 let goods = [
   {
     id: 0,
-    name: 'Hot dog',
+    name: "Hot dog",
     price: 30,
-    imgUrl: '../images/',
+    imgUrl: "images/dogg.png",
   },
   {
     id: 1,
-    name: 'Donut',
+    name: "Donut",
     price: 20,
-    imgUrl: '../images/',
+    imgUrl: "images/dogg.png",
   },
   {
     id: 2,
-    name: 'Sauces',
+    name: "Sauces",
     price: 10,
-    imgUrl: '../images/',
+    imgUrl: "images/dogg.png",
   },
   {
     id: 3,
-    name: 'Pizza',
+    name: "Pizza",
     price: 30,
-    imgUrl: '../images/',
+    imgUrl: "images/dogg.png",
   },
   {
     id: 4,
-    name: 'Burger',
+    name: "Burger",
     price: 30,
-    imgUrl: '../images/',
-  }
+    imgUrl: "images/2.png",
+  },
 ];
+
+renderFoodsItems(goods);
+
+/*Якась важлива штука, без якої не працює драг */
+trayBox.addEventListener("dragover", function (event) {
+  event.preventDefault();
+});
+
+/*Додавання товару до массиву подноса */
+function addFoodToTray(dragEl) {
+  let id = dragEl.getAttribute("data-id");
+  let name = dragEl.querySelector(".app__food-name").textContent;
+  let price = dragEl.querySelector(".app__food-price-num").textContent;
+  let imgUrl = dragEl.querySelector(".app__food-img").getAttribute("src");
+  let newFood = {
+    id: id,
+    name: name,
+    price: price,
+    imgUrl: imgUrl,
+  };
+  trayArr.push(newFood);
+}
+
+
+/*Логіка додавання в корзину */
+function addToTray(e) {
+  e.preventDefault();
+  const dragEl = document.querySelector(".drag");
+  // const img = dragEl.querySelector("img");
+  // let clone = img.cloneNode(true);
+  // trayInner.appendChild(clone);
+  trayBox.classList.add("pulse");
+  setTimeout(() => {
+    trayBox.classList.remove("pulse");
+  }, 400);
+
+  addFoodToTray(dragEl);
+  calcPrice(trayArr);
+  renderFoodsInTray(trayArr);
+}
+
+
+/*Функція рендеру товарів з массиву доданих товарів в корзину */
+function renderFoodsInTray(arr) {
+  let foodsHtml = arr.map((item) => {
+    return `
+          <img src="${item.imgUrl}" alt="food" class="app__food-img" />
+    `;
+  });
+  foodsHtml = foodsHtml.join(" ");
+  trayInner.innerHTML = foodsHtml;
+}
+
+
+function calcPrice(arr) {
+  let totalPrice = 0;
+  arr.forEach(item => {
+    totalPrice += +item.price; 
+  });
+  totalPriceNode.innerHTML = totalPrice;
+}
+
+/*Функція рендеру товарів */
+function renderFoodsItems(arr) {
+  let foodsHtml = arr.map((item) => {
+    return `
+                <li data-id="${item.id}" draggable="true" class="app__food-item">
+              <img src="${item.imgUrl}" alt="food" class="app__food-img" />
+              <h3 class="app__food-name">${item.name}</h3>
+              <p class="app__food-price"><span class="app__food-price-num">${item.price}</span> <span>UAH</span></p>
+              <div class="app__food-mobile__add-box">
+                <div class="app__food-mobile__add">
+                  <button class="app__food-mobile__add-btn">Додати</button>
+                </div>
+              </div>
+            </li>
+    `;
+  });
+  foodsHtml = foodsHtml.join(" ");
+  foodsList.innerHTML = foodsHtml;
+
+  let foods = document.querySelectorAll(".app__food-item");
+  foods.forEach((food) => {
+    /*Додаєм клас елементу, якого перетягуєм, для його ідентифікації*/
+    food.addEventListener("dragstart", () => {
+      food.classList.add("drag");
+    });
+    /*Забираєм клас елементу, якого перетягуєм*/
+    food.addEventListener("dragend", () => {
+      food.classList.remove("drag");
+    });
+  });
+  trayBox.addEventListener("drop", addToTray);
+}
